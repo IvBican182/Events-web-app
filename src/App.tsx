56445c15d8999320db72682b3efddaaa274db409
IvBican182 <ibicanic@live.com>
2024-07-08 16:31:03 +0200

    setting up css modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+import Footer from "./components/pageFooter/Footer"
+import Navbar from "./components/pageNavbar/Navbar"
 
 
 function App() {
---
+++ src/components/pageFooter/Footer.tsx
@@ -0,0 +1,14 @@
+import git_logo from "../../assets/git-logo.png";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+    return (
+        <div className={styles.footer}>
+            <a className={styles.link}>
+                <img src={git_logo} alt="github logo"/>
+                <p>Developed by IvBican182</p>
+            </a>
+            <p className={styles.copyright}>© Copyright 2024 Ivan Bićanić. All rights reserved.</p>
+        </div>
+    )
+}
